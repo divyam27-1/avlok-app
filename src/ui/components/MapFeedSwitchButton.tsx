457bc00b9a0b5ae430f07659c231e7efcdfe7b07
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-01-20 12:47:09 +0530

    style: Remove the blue border from the Map Feed button

--- src/ui/components/MapFeedSwitchButton.tsx
+++ src/ui/components/MapFeedSwitchButton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MapFeedSwitchButton = () => {
   return (
-      <button type='button' className='absolute z-10 rounded-3xl btn bg-white hover:bg-gray-200 focus:ring-black top-20 left-20'>
+      <button type='button' className='absolute z-10 rounded-3xl btn bg-white hover:bg-gray-200 focus:ring-black top-20 left-20 !outline-none border-none focus:ring-0'>
       <img src="src/ui/assets/locationIcon.png" alt="" width={60}/>
     </button>
   )

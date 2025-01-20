cf5e1b18215daebf74593d49f152feb1b3e9c004
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-01-20 12:45:14 +0530

    style: Position the Map Feed button

--- src/ui/components/MapFeedSwitchButton.tsx
+++ src/ui/components/MapFeedSwitchButton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MapFeedSwitchButton = () => {
   return (
-      <button type='button' className='rounded-md btn bg-white hover:bg-gray-200 focus:outline-none focus:border-none hover:outline-none border-transparent focus:border-transparent focus:ring-0'>
+      <button type='button' className='absolute z-10 rounded-3xl btn bg-white hover:bg-gray-200 focus:ring-black top-20 left-20'>
       <img src="src/ui/assets/locationIcon.png" alt="" width={60}/>
     </button>
   )

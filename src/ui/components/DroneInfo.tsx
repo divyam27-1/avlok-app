d5bcd7fb5a4b1cb4ac07ef564d90b97a2bcde7ae
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-01-20 12:52:31 +0530

    style: Add drone image for the button

---
+++ src/ui/components/DroneInfo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DroneInfo = () => {
+  return (
+      <button type='button' className='absolute z-10 rounded-3xl btn bg-white hover:bg-gray-200 focus:ring-black top-20 right-20 !outline-none border-none focus:ring-0'>
+      <img src="src/ui/assets/droneIcon.png" alt="" width={60}/>
+    </button>
+  )
+}
+
+export default DroneInfo
--- src/ui/pages/CameraFeed.tsx
+++ src/ui/pages/CameraFeed.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Webcam from '../components/Webcam'
 import MapFeedSwitchButton from '../components/MapFeedSwitchButton'
+import DroneInfo from '../components/DroneInfo'
 
 const CameraFeed = () => {
   return (
     <div className='relative'>
         <MapFeedSwitchButton></MapFeedSwitchButton>
+        <DroneInfo></DroneInfo>
         <Webcam></Webcam>
     </div>
   )

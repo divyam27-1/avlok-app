a5cedc8b038e44f35ed1b87ce40576f41dc7a032
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-01-20 16:27:19 +0530

    style: Change the font size and paddings between items inside sidebar and fill sidebar with dummy data

--- src/ui/components/DroneInformationSidebar.tsx
+++ src/ui/components/DroneInformationSidebar.tsx
@@ -13,14 +13,31 @@ const DroneInformationSidebar = ({isOpen, onClose} : Props) => {
       isOpen ? "translate-x-0" : "translate-x-full"
     } transition-transform duration-300`}
   >
+    <h2 className='text-xl pt-6 font-bold px-2'>Drone Information</h2>
     <button
           onClick={onClose}
-          className="absolute top-4 left-4 text-gray-400 hover:text-white"
+          className="absolute top-4 right-4 text-gray-400 hover:text-white"
         >
           ×
         </button>
     <div className='p-6'>
-
+        <div className='py-3'>
+            <h3 className='text-xl italic'>Latitude</h3>
+        <p>45.3</p>
+        </div>
+        <div className='py-3'>
+            <h3 className='text-xl italic'>Longitude</h3>
+        <p>21.3</p>
+        </div><div className='py-3'>
+            <h3 className='text-xl italic'>Altitude</h3>
+        <p>20.0</p>
+        </div><div className='py-3'>
+            <h3 className='text-xl italic'>Other Parameter 1</h3>
+        <p>21.3</p>
+        </div><div className='py-3'>
+            <h3 className='text-xl italic'>Other Parameter 2</h3>
+        <p>21.3</p>
+        </div>
     </div>
     </div>
     {isOpen && (

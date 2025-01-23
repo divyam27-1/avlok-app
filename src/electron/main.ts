76dd70ed729e7ba6db2a22dc1cf16d2e57c885f5
Divyam_Tiwari <divyam2701@gmail.com>
2025-01-23 11:50:23 +0530

    bugfix: fixed a little bug about electron server not running on the same port as the react server, otherwise its fine

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -5,7 +5,7 @@ import {isDev} from './utils.js';
 app.on("ready", ()=>{
     const mainWindow = new BrowserWindow({});
     if (isDev()){
-        mainWindow.loadURL("http://localhost:199/");
+        mainWindow.loadURL("http://localhost:5173/");
     } else {
         mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
     }

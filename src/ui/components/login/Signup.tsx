0deaa4c9cfb0c7b50b5034342b1d5ef0c210b7d7
SATISH PATI <satishpati2006@gmail.com>
2025-06-07 21:00:06 +0530

    Update Signup.tsx

--- src/ui/components/login/Signup.tsx
+++ src/ui/components/login/Signup.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './signup.css';
+import { useNavigate } from 'react-router-dom';
 
 function Signup() {
   return (
@@ -10,19 +11,16 @@ function Signup() {
 
       <div className="login-container">
         <h2>Create Account</h2>
-
         <div className="form-group">
-          <img src="src/ui/assets/icons/user.svg" height="22" width="20" className="minilogo" />
+        <img src="src/ui/assets/user.svg" alt="Password" className="w-5 h-5 ml-4  mr-4" />
           <input type="text" placeholder="Name" required />
         </div>
-
         <div className="form-group">
-          <img src="src/ui/assets/icons/envelope.svg" height="22" width="20" className="minilogo" />
+      <img src="src/ui/assets/envelope.svg" alt="Password" className="w-5 h-5 ml-4 mr-4" />
           <input type="email" placeholder="Email" required />
         </div>
-
         <div className="form-group">
-          <img src="src/ui/assets/icons/lock.svg" height="22" width="20" className="minilogo" />
+                    <img src="src/ui/assets/lock.svg" alt="Password" className="w-5 h-5 ml-4  mr-4" />
           <input type="password" placeholder="Password" required />
         </div>
 

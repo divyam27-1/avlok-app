2a098ea017fbf45448ae1c6ef84ef109a16b2c2c
SATISH PATI <satishpati2006@gmail.com>
2025-06-07 20:58:14 +0530

    Update login.tsx

--- src/ui/components/login/login.tsx
+++ src/ui/components/login/login.tsx
@@ -1,19 +1,19 @@
 // src/components/Login.jsx
 import './Login.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+    const navigate = useNavigate();
   return (
     <div className="background-container">
-      {/* Logo */}
       <img src="src/ui/assets/iittplogo.jpg" alt="IIT Tirupati Logo" className="logo" />
       <img src="src/ui/assets/lg.svg" alt="IIT Tirupati Logo 2" className="logo2" />
 
-      {/* Login Card */}
       <div className="login-card">
         <h1 className="title">IIT TIRUPATI</h1>
 
-        <button className="login-button">ADMIN LOGIN</button>
-        <button className="login-button">SECURITY LOGIN</button>
+        <button className="login-button"  onClick={() => navigate('/loginReg')}>ADMIN LOGIN </button>
+        <button className="login-button" onClick={() => navigate('/loginReg')}>SECURITY LOGIN</button>
 
         <p className="signup-text">Don’t have an account at?</p>
         <button className="signup-button">Sign up</button>

25f2a48a129300affd581833011424f0ce84a869
satish pati <satishpati2006@gmail.com>
2025-06-07 10:45:59 +0530

    Updated App.tsx

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,12 +2,31 @@ import './App.css'
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import CameraFeed from './pages/CameraFeed';
 import MapFeed from './pages/MapFeed';
-
+import Login from './components/login/login';
+import LoginReg from './components/login/LoginReg';
+import VerifyOtp from './components/login/VerifyOTP';
+import SetPassword from './components/login/SetPassword';
+import Signup from './components/login/Signup';
+import DroneControlInterface from './components/drone/dronecontrol';
+import AuthorityControlPanel from './components/drone/AuthorityControl';
+import AlertStatusScreen from './components/drone/alert';
+import TelemetryDashboard from './components/drone/telemetry';
+import MapNavigationView from './components/drone/MapNav';
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<CameraFeed></CameraFeed>}></Route>
+                   <Route path="/" element={< MapNavigationView/>}/>
+        <Route path="/tele" element={<TelemetryDashboard/>}/>
+      <Route path="/alert" element={<AlertStatusScreen/>}/>
+      <Route path="/authority" element={<AuthorityControlPanel/>}/>
+      <Route path="/dronecontrol" element={<DroneControlInterface />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/setpassword" element={<SetPassword />} />
+      <Route path="/verifyOtp" element={<VerifyOtp />} />
+      <Route path="/loginReg" element={<LoginReg />} />
+      <Route path='/camera' element={<CameraFeed></CameraFeed>}></Route>
       <Route path='/map' element={<MapFeed></MapFeed>}></Route>
     </Routes>
     </BrowserRouter>
